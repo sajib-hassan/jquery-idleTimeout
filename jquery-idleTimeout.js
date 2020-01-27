04b445616db909fc601e235d07955eae925235c8
Sajib Hassan <sajib.hassan@gmail.com>
2020-01-27 16:08:36 +0600

    Fixed the bugs

--- jquery-idleTimeout.js
+++ jquery-idleTimeout.js
@@ -26,53 +26,54 @@
     //## Public Configuration Variables
     //##############################
     var defaultConfig = {
-      redirectUrl: '/logout',      // redirect to this url on logout. Set to "redirectUrl: false" to disable redirect
+          redirectUrl: '/logout',      // redirect to this url on logout. Set to "redirectUrl: false" to disable redirect
 
-      // idle settings
-      idleTimeLimit: 1200,           // 'No activity' time limit in seconds. 1200 = 20 Minutes
-      idleCheckHeartbeat: 2,       // Frequency to check for idle timeouts in seconds
+          // idle settings
+          idleTimeLimit: 1200,           // 'No activity' time limit in seconds. 1200 = 20 Minutes
+          idleCheckHeartbeat: 2,       // Frequency to check for idle timeouts in seconds
 
-      // optional stayLoggedIn callback to perform after stayLoggedIn button click
-      stayLoggedInCallback: false,       // set to false for no customCallback
+          // optional stayLoggedIn callback to perform after stayLoggedIn button click
+          stayLoggedInCallback: false,       // set to false for no customCallback
 
-      // optional custom callback to perform before logout
-      customCallback: false,       // set to false for no customCallback
-      // customCallback:    function () {    // define optional custom js function
+          // optional custom callback to perform before logout
+          customCallback: false,       // set to false for no customCallback
+          // customCallback:    function () {    // define optional custom js function
           // perform custom action before logout
-      // },
+          // },
 
-      // configure which activity events to detect
-      // http://www.quirksmode.org/dom/events/
-      // https://developer.mozilla.org/en-US/docs/Web/Reference/Events
-      activityEvents: 'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
+          // configure which activity events to detect
+          // http://www.quirksmode.org/dom/events/
+          // https://developer.mozilla.org/en-US/docs/Web/Reference/Events
+          activityEvents: 'click keypress scroll wheel mousewheel mousemove', // separate each event with a space
 
-      // warning dialog box configuration
-      enableDialog: true,           // set to false for logout without warning dialog
-      dialogDisplayLimit: 180,       // Time to display the warning dialog before logout (and optional callback) in seconds. 180 = 3 Minutes
-      dialogTitle: 'Session Expiration Warning', // also displays on browser title bar
-      dialogText: 'Because you have been inactive, your session is about to expire.',
-      dialogTimeRemaining: 'Time remaining',
-      dialogStayLoggedInButton: 'Stay Logged In',
-      dialogLogOutNowButton: 'Log Out Now',
+          // warning dialog box configuration
+          enableDialog: true,           // set to false for logout without warning dialog
+          dialogDisplayLimit: 180,       // Time to display the warning dialog before logout (and optional callback) in seconds. 180 = 3 Minutes
+          dialogTitle: 'Session Expiration Warning', // also displays on browser title bar
+          dialogText: 'Because you have been inactive, your session is about to expire.',
+          dialogTimeRemaining: 'Time remaining',
+          dialogStayLoggedInButton: 'Stay Logged In',
+          dialogLogOutNowButton: 'Log Out Now',
+          dialogCssScopeClassName: false,
 
-      // error message if https://github.com/marcuswestin/store.js not enabled
-      errorAlertMessage: 'Please disable "Private Mode", or upgrade to a modern browser. Or perhaps a dependent file missing. Please see: https://github.com/marcuswestin/store.js',
+          // error message if https://github.com/marcuswestin/store.js not enabled
+          errorAlertMessage: 'Please disable "Private Mode", or upgrade to a modern browser. Or perhaps a dependent file missing. Please see: https://github.com/marcuswestin/store.js',
 
-      // server-side session keep-alive timer
-      sessionKeepAliveTimer: 600,   // ping the server at this interval in seconds. 600 = 10 Minutes. Set to false to disable pings
-      sessionKeepAliveUrl: window.location.href // set URL to ping - does not apply if sessionKeepAliveTimer: false
-    },
+          // server-side session keep-alive timer
+          sessionKeepAliveTimer: 600,   // ping the server at this interval in seconds. 600 = 10 Minutes. Set to false to disable pings
+          sessionKeepAliveUrl: window.location.href // set URL to ping - does not apply if sessionKeepAliveTimer: false
+        },
 
-    //##############################
-    //## Private Variables
-    //##############################
-      currentConfig = $.extend(defaultConfig, userRuntimeConfig), // merge default and user runtime configuration
-      origTitle = document.title, // save original browser title
-      activityDetector,
-      startKeepSessionAlive, stopKeepSessionAlive, keepSession, keepAlivePing, // session keep alive
-      idleTimer, remainingTimer, checkIdleTimeout, checkIdleTimeoutLoop, startIdleTimer, stopIdleTimer, // idle timer
-      openWarningDialog, dialogTimer, checkDialogTimeout, startDialogTimer, stopDialogTimer, isDialogOpen, destroyWarningDialog, countdownDisplay, // warning dialog
-      logoutUser;
+        //##############################
+        //## Private Variables
+        //##############################
+        currentConfig = $.extend(defaultConfig, userRuntimeConfig), // merge default and user runtime configuration
+        origTitle = document.title, // save original browser title
+        activityDetector,
+        startKeepSessionAlive, stopKeepSessionAlive, keepSession, keepAlivePing, // session keep alive
+        idleTimer, remainingTimer, checkIdleTimeout, checkIdleTimeoutLoop, startIdleTimer, stopIdleTimer, // idle timer
+        openWarningDialog, dialogTimer, checkDialogTimeout, startDialogTimer, stopDialogTimer, isDialogOpen, destroyWarningDialog, countdownDisplay, // warning dialog
+        logoutUser;
 
     //##############################
     //## Public Functions
@@ -157,7 +158,7 @@
 
       var dialogContent = "<div id='idletimer_warning_dialog'><p>" + currentConfig.dialogText + "</p><p style='display:inline'>" + currentConfig.dialogTimeRemaining + ": <div style='display:inline' id='countdownDisplay'></div></p></div>";
 
-      $(dialogContent).dialog({
+      var d = $(dialogContent).dialog({
         buttons: [{
           text: currentConfig.dialogStayLoggedInButton,
           click: function () {
@@ -172,17 +173,33 @@
           {
             text: currentConfig.dialogLogOutNowButton,
             click: function () {
+              destroyWarningDialog();
+              stopDialogTimer();
               logoutUser();
             }
           }
-          ],
+        ],
         closeOnEscape: false,
         modal: true,
+        autoOpen: false,
+        draggable: false,
+        width: 500,
+        resizable: false,
         title: currentConfig.dialogTitle,
+        close: function() {
+          if(currentConfig.dialogCssScopeClassName){
+            $('body').removeClass(currentConfig.dialogCssScopeClassName);
+          }
+        },
         open: function () {
           $(this).closest('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+          if(currentConfig.dialogCssScopeClassName){
+            $('body').addClass(currentConfig.dialogCssScopeClassName);
+          }
         }
       });
+
+      d.dialog('open');
 
       countdownDisplay();
 
@@ -232,6 +249,7 @@
       var dialogDisplaySeconds = currentConfig.dialogDisplayLimit, mins, secs;
 
       remainingTimer = setInterval(function () {
+        if (dialogDisplaySeconds < 0) { return;}
         mins = Math.floor(dialogDisplaySeconds / 60); // minutes
         if (mins < 10) { mins = '0' + mins; }
         secs = dialogDisplaySeconds - (mins * 60); // seconds
@@ -249,6 +267,10 @@
         stopKeepSessionAlive();
       }
 
+      if(store.get('fireIdleTimerLoggedOutCallback') === true){ return; }
+
+      store.set('fireIdleTimerLoggedOutCallback', true);
+
       if (currentConfig.customCallback) {
         currentConfig.customCallback();
       }
@@ -256,6 +278,7 @@
       if (currentConfig.redirectUrl) {
         window.location.href = currentConfig.redirectUrl;
       }
+
     };
 
     //###############################
@@ -268,6 +291,7 @@
 
         store.set('idleTimerLastActivity', $.now());
         store.set('idleTimerLoggedOut', false);
+        store.set('fireIdleTimerLoggedOutCallback', false);
 
         activityDetector();
 
